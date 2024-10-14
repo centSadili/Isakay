ac13b0b3e08a9a684cc32503172562575389f6cf
centSadili <sadiliva@students.nu-moa.edu.ph>
2024-10-14 18:01:14 +0800

    Error fixed

--- rentalvehiclesystem/src/Components/Signup/SignUp.jsx
+++ rentalvehiclesystem/src/Components/Signup/SignUp.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {Link,useNavigate} from 'react-router-dom'
 import axios from 'axios'
 import './SignUp.css';
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock } from 'react-icons/fa';
 
 const SignUp = () => {
     const [data,setData]=useState({

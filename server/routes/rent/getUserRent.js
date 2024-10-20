b92d3ef124dc21837932f4cbd433ec55a6ebbddc
centSadili <sadiliva@students.nu-moa.edu.ph>
2024-10-20 22:33:37 +0800

    modify: getUserRent

--- server/routes/rent/getUserRent.js
+++ server/routes/rent/getUserRent.js
@@ -4,10 +4,11 @@ const PersonalDetails = require('../../models/personalDetail');
 const RentDetail = require('../../models/rentDetail');
 
 router.get('/user-rent-details/:userId', async (req, res) => {
+    
     try {
         const userId = req.params.userId;
 
-        // Find all personal details of the user (assuming user can have multiple personal details)
+      
         const personalDetailsList = await PersonalDetails.find({ user: userId });
 
         if (!personalDetailsList || personalDetailsList.length === 0) {
@@ -16,32 +17,32 @@ router.get('/user-rent-details/:userId', async (req, res) => {
 
         let allRentDetails = [];
 
-        // Loop through all personalDetails entries to find all associated rent details
+      
         for (const personalDetails of personalDetailsList) {
             const rentDetails = await RentDetail.find({ renterID: personalDetails._id })
                 .populate({
                     path: 'renterID',
-                    model: 'PersonalDetails', // Ensure this is the correct model name
+                    model: 'PersonalDetails', 
                 })
                 .populate({
                     path: 'carID',
-                    model: 'Car', // Ensure this is the correct model name
+                    model: 'Car', 
                 })
                 .populate({
                     path: 'transactionID',
-                    model: 'TransactDetails', // Correct model name
+                    model: 'TransactDetails', 
                 })
                 .exec();
 
             allRentDetails = allRentDetails.concat(rentDetails);
         }
 
-        // If no rent details found for any personalDetails
+
         if (allRentDetails.length === 0) {
             return res.status(404).json({ error: 'No rent details found for the user' });
         }
 
-        // Return all rent details along with personal details
+
         res.status(200).json({
             personalDetails: personalDetailsList,
             rentDetails: allRentDetails,

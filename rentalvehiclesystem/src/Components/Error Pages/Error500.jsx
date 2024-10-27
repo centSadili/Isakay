e497ca66208c1c700d81239f876fd7cfb65b3a1d
Elijah <elijahgundayao10@gmail.com>
2024-10-27 17:43:38 +0800

    Error pages fix

--- rentalvehiclesystem/src/Components/Error Pages/Error500.jsx
+++ rentalvehiclesystem/src/Components/Error Pages/Error500.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Result } from 'antd';
 import { Link } from 'react-router-dom';
-
+import './Error.css'
 
 const Error500 = () => (
   <Result
@@ -9,7 +9,7 @@ const Error500 = () => (
     title="500"
     subTitle="Sorry, but you need to log in first."
     extra={<Link to="/login">
-        <Button type="primary">Log in</Button>
+        <Button type="primary">Click to Log In</Button>
       </Link>}
   />
 );

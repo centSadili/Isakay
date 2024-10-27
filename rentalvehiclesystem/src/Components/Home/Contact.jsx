99f401dc049be0c22b9f379e6030bf8de54b6bd2
Stibbop <stevensulayao@gmail.com>
2024-10-27 22:07:11 +0800

    did carlist, carpage, header,about us, contact us and carlogotrialpng

--- rentalvehiclesystem/src/Components/Home/Contact.jsx
+++ rentalvehiclesystem/src/Components/Home/Contact.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../Header/Header'
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>
-      <Header/>
+    <div className="contact-main-container">
+      <Header />
+
+      <div className="contact-container">
+        <h1 className="contact-title">Contact Us</h1>
+       
+      </div>
     </div>
   )
 }

7062e2a8f75e0a42ad0d0ced527b4539fb782fd3
Stibbop <stevensulayao@gmail.com>
2024-10-31 20:52:36 +0800

    fix header about and search car

--- rentalvehiclesystem/src/Components/Home/About.jsx
+++ rentalvehiclesystem/src/Components/Home/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './About.css';
+import Footer from '../Footer/Footer';
 
 const About = () => {
   return (
@@ -25,12 +26,12 @@ const About = () => {
         <h2 className="contact-title2">GALLERY</h2>
         
         <div class="gallery">
-            <img src="https://i.pinimg.com/736x/9c/fb/8f/9cfb8f39d2db9e4a45b972b93e59bd17.jpg" alt="Gallery Image 1" />
-            <img src="https://i.pinimg.com/564x/11/26/77/112677978c3d6b59a418677161c9623d.jpg" alt="Gallery Image 2" />
-            <img src="https://i.pinimg.com/564x/2c/fa/c3/2cfac3409ca7ca0762ad7a3b8833eb28.jpg" alt="Gallery Image 3" />
-            <img src="https://i.pinimg.com/736x/98/ea/32/98ea32fb48a95d43ca5481490544a9b0.jpg" alt="Gallery Image 4" />
-            <img src="https://i.pinimg.com/564x/10/48/2e/10482e8df1e76560be73a974cb463c3d.jpg" alt="Gallery Image 5" />
-            <img src="https://i.pinimg.com/564x/a2/bc/e2/a2bce2804ecacbc7ce7892352ebd1290.jpg" alt="Gallery Image 6" />
+            <img src="https://images.unsplash.com/photo-1605556816125-d752c226247b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDU5fHxzdXBlciUyMGNhcnxlbnwwfHwwfHx8MA%3D%3D" alt="Gallery Image 1" />
+            <img src="https://images.unsplash.com/photo-1628188422678-88d25bf27bed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D" alt="Gallery Image 2" />
+            <img src="https://images.unsplash.com/photo-1616591938558-fb03d845567b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D" alt="Gallery Image 3" />
+            <img src="https://images.unsplash.com/photo-1616591938203-9b5630a01006?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3VwZXJjYXJ8ZW58MHx8MHx8fDA%3D" alt="Gallery Image 4" />
+            <img src="https://images.unsplash.com/photo-1635770310909-f5569b8f4b95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIzfHx8ZW58MHx8fHx8" alt="Gallery Image 5" />
+            <img src="https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-1fndzcd_41017374.jpeg?region=0,0,600,600" alt="Gallery Image 6" />
       </div>
          
       <h3 class="abouts"> Travel with us on another world! – Isakay</h3>
@@ -47,13 +48,10 @@ const About = () => {
   Our vision is to be the leading provider of transportation rental services in the region, known for innovation, exceptional customer service, and a commitment to sustainability. We aspire to be a trusted partner in local mobility, setting new standards for quality and convenience in every ride, and building a community where travel is always within reach.
 </p>
 
-
-
-      
         </section>
 
-
       </div>
+      <Footer/>
 
         
 

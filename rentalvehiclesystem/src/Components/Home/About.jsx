cce1f7526fcb2525f3cd66f95a07065ac7f58cb9
Stibbop <stevensulayao@gmail.com>
2024-11-01 16:19:23 +0800

    responsive about us

--- rentalvehiclesystem/src/Components/Home/About.jsx
+++ rentalvehiclesystem/src/Components/Home/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Header/Header'
 import './About.css';
 import Footer from '../Footer/Footer';
+import Logo from "../../images/main.png"
 
 const About = () => {
   return (
@@ -9,9 +10,9 @@ const About = () => {
       <Header/>
       <div className="about-container">
       <h1 className="about-title">About Us</h1>
-      <section id="contact-things-about-us" className="contact-section">
-          <img className="contact-profile-img" src="carlogotrial.png" alt="Your Name - Photographer" />
-          <h2 className="contact-title2">Isakay</h2>
+      <section  className="contact-section">
+          <img className="contact-profile-img" src={Logo} alt="Your Name - Photographer" />
+          <h2 className="contact-title2">Isakay Rental</h2>
           <h2 className="get"style={{ marginTop: '20px' }}>GET TO KNOW US</h2>
           <p className="contact-text">
           Welcome to Isakay Rental, your reliable partner for convenient and affordable transportation solutions. At Isakay Rental, we are passionate about making local travel easier, more accessible, and enjoyable. Whether you're exploring the vibrant cityscape, visiting family, or embarking on an exciting adventure, our fleet is designed to meet your every need.
@@ -22,16 +23,16 @@ const About = () => {
     </p>
         </section>
         
-        <section id="contact-things-about-us" className="contact-section">
+        <section  className="contact-section">
         <h2 className="contact-title2">GALLERY</h2>
         
         <div class="gallery">
             <img src="https://images.unsplash.com/photo-1605556816125-d752c226247b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDU5fHxzdXBlciUyMGNhcnxlbnwwfHwwfHx8MA%3D%3D" alt="Gallery Image 1" />
-            <img src="https://images.unsplash.com/photo-1628188422678-88d25bf27bed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D" alt="Gallery Image 2" />
-            <img src="https://images.unsplash.com/photo-1616591938558-fb03d845567b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D" alt="Gallery Image 3" />
-            <img src="https://images.unsplash.com/photo-1616591938203-9b5630a01006?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3VwZXJjYXJ8ZW58MHx8MHx8fDA%3D" alt="Gallery Image 4" />
-            <img src="https://images.unsplash.com/photo-1635770310909-f5569b8f4b95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIzfHx8ZW58MHx8fHx8" alt="Gallery Image 5" />
-            <img src="https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-1fndzcd_41017374.jpeg?region=0,0,600,600" alt="Gallery Image 6" />
+            <img src="https://images.unsplash.com/photo-1702565085670-419305fe1d43?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTY4fHxzdXBlcmNhcnN8ZW58MHx8MHx8fDA%3D" alt="Gallery Image 2" />
+            <img src="https://images.unsplash.com/photo-1691737833142-f41bed29717c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjAwfHxzdXBlcmNhcnN8ZW58MHx8MHx8fDA%3D" alt="Gallery Image 3" />
+            <img src="https://images.unsplash.com/photo-1716976105483-6c5f9ec2f0c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM2fHxzdXBlcmNhcnN8ZW58MHx8MHx8fDA%3D" alt="Gallery Image 4" />
+            <img src="https://images.unsplash.com/photo-1644860112235-54bf565263f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHN1cGVyY2Fyc3xlbnwwfHwwfHx8MA%3D%3D" alt="Gallery Image 5" />
+            <img src="https://images.unsplash.com/photo-1716976106458-b5d79d597c5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3VwZXJjYXJzfGVufDB8fDB8fHww" alt="Gallery Image 6" />
       </div>
          
       <h3 class="abouts"> Travel with us on another world! – Isakay</h3>
